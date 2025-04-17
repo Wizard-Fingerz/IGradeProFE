71eeec37ecaca655b0bc3ce8d026b01ebf903469
WizCode <adewale.oladiti28@gmail.com>
2025-04-17 03:20:00 +0100

    `Added modal for viewing exam details and updated menu actions`

--- src/pages/MarkExam/index.tsx
+++ src/pages/MarkExam/index.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography, Avatar, MenuItem, Menu, IconButton, Grid, Button } from '@mui/material';
+import { Box, Typography, Avatar, MenuItem, Menu, IconButton, Grid, Button, Dialog, DialogTitle, DialogContent, DialogActions, Stack } from '@mui/material';
 import CustomInput from '../../components/CustomBorderedInput';
 import { getProfileDetails } from '../../services/auth/profile';
 import { User } from '../../types/user';
 import CustomDataTable from '../../components/CustomTable/mui';
 import { Link, useNavigate } from 'react-router-dom';
-import { MoreVert } from '@mui/icons-material';
+import { MoreVert, Visibility } from '@mui/icons-material';
 import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
 import AnalyticsCard from '../../components/AnalyticsCard';
 import { fetchAllResultsWithPagination } from '../../services/results';
@@ -14,14 +14,19 @@ const MarkExamPage: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const [data, setData] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-  const [,setHasNextPage] = React.useState(false);
-  const [,setHasPreviousPage] = React.useState(false);
-  const [networkError, setNetworkError] = React.useState(false); // New state for network error
+  const [, setHasNextPage] = React.useState(false);
+  const [, setHasPreviousPage] = React.useState(false);
+  const [networkError, setNetworkError] = React.useState(false);
   const [dataCount, setDataCount] = React.useState(0);
   const [paginationModel, setPaginationModel] = React.useState({ pageSize: 15, page: 0, currentPage: 1 });
   const navigate = useNavigate();
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [selectedRowId, setSelectedRowId] = React.useState<string | null>(null);
+  const [selectedRowData, setSelectedRowData] = React.useState<any>(null); // State for selected row data
+  const [isModalOpen, setIsModalOpen] = React.useState(false); // State for modal open/close
 
+  const isMenuOpen = Boolean(anchorEl);
 
   useEffect(() => {
     const fetchStudentMe = async () => {
@@ -33,9 +38,6 @@ const MarkExamPage: React.FC = () => {
     fetchStudentMe();
   }, []);
 
-
-
-
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
@@ -43,13 +45,12 @@ const MarkExamPage: React.FC = () => {
 
       try {
         const response = await fetchAllResultsWithPagination({
-          pageIndex: paginationModel.page, // 0-based for backend
+          pageIndex: paginationModel.page,
           pageSize: paginationModel.pageSize,
-          currentPage: paginationModel.currentPage, // 1-based current page
+          currentPage: paginationModel.currentPage,
         });
 
         setData(response.results);
-
         setDataCount(response.count);
         setHasNextPage(!!response.next);
         setHasPreviousPage(!!response.previous);
@@ -64,30 +65,27 @@ const MarkExamPage: React.FC = () => {
     fetchData();
   }, [paginationModel]);
 
-
-
   const handlePaginationModelChange = (model: { pageSize: number; page: number }) => {
-    const newPage = model.page + 1; // Convert 0-based index to 1-based currentPage
-    setPaginationModel(prev => ({ ...prev, page: model.page, pageSize: model.pageSize, currentPage: newPage }));
+    const newPage = model.page + 1;
+    setPaginationModel((prev) => ({ ...prev, page: model.page, pageSize: model.pageSize, currentPage: newPage }));
   };
 
+  const handleView = (row: any) => {
+    setSelectedRowData(row); // Set the selected row data
+    setIsModalOpen(true); // Open the modal
+  };
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  // Add a state to keep track of the selected row ID
-  const [selectedRowId, setSelectedRowId] = React.useState<string | null>(null);
-
-
-
-  const isMenuOpen = Boolean(anchorEl); // Update this line
-
-
+  const handleCloseModal = () => {
+    setIsModalOpen(false); // Close the modal
+    setSelectedRowData(null); // Clear the selected row data
+  };
 
   const columns: GridColDef[] = [
     {
       field: 'candidate_number',
       headerName: 'Candidate Number',
       flex: 1,
-      type: 'string', // Specify the type here
+      type: 'string',
       renderCell: (params: GridRenderCellParams) => (
         <Link to={'#'}>
           <div>{params.value}</div>
@@ -101,10 +99,7 @@ const MarkExamPage: React.FC = () => {
       type: 'string',
       renderCell: (params: GridRenderCellParams) => (
         <Link to={'#'}>
-          <div>
-            {params.value}
-
-          </div>
+          <div>{params.value}</div>
         </Link>
       ),
     },
@@ -130,7 +125,6 @@ const MarkExamPage: React.FC = () => {
         </Link>
       ),
     },
-
     {
       field: 'similarity_score_percentage',
       headerName: 'Similarity Score',
@@ -142,22 +136,18 @@ const MarkExamPage: React.FC = () => {
         </Link>
       ),
     },
-
-    // Repeat for other columns...
-
     {
       field: 'actions',
       headerName: 'Actions',
-      flex: 0.5, // Fixed width
+      flex: 0.5,
       renderCell: (params: GridRenderCellParams) => {
         const handleClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-          // Handle the click to open the menu
           setAnchorEl(event.currentTarget);
-          setSelectedRowId(params.row.id); // Store the selected row ID
+          setSelectedRowId(params.row.id);
         };
 
         const handleClose = () => {
-          setAnchorEl(null); // Close the menu
+          setAnchorEl(null);
         };
 
         return (
@@ -165,13 +155,17 @@ const MarkExamPage: React.FC = () => {
             <IconButton onClick={handleClick}>
               <MoreVert />
             </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              open={isMenuOpen && selectedRowId === params.row.id} // Check against selectedRowId
-              onClose={handleClose}
-            >
-              <MenuItem>Edit</MenuItem>
-              <MenuItem>Delete</MenuItem>
+            <Menu anchorEl={anchorEl} open={isMenuOpen && selectedRowId === params.row.id} onClose={handleClose}>
+              <MenuItem onClick={() => handleView(params.row)}>
+
+                <span
+                  className='flex flex-row gap-4 text-blue-800'
+
+                >
+                  <Visibility width={20} />
+                  <p>View</p>
+                </span>
+              </MenuItem>
             </Menu>
           </div>
         );
@@ -179,6 +173,18 @@ const MarkExamPage: React.FC = () => {
     },
   ];
 
+
+  const DetailRow = ({ label, value }: { label: string; value: any }) => (
+    <Box>
+      <Typography variant="subtitle2" color="text.secondary">
+        {label}
+      </Typography>
+      <Typography variant="body1" color="text.primary">
+        {value || <em>N/A</em>}
+      </Typography>
+    </Box>
+  );
+  
 
   return (
     <Box>
@@ -188,33 +194,26 @@ const MarkExamPage: React.FC = () => {
           sx={{
             fontSize: 28,
             fontWeight: 600,
-            marginBottom: { xs: 2, md: 0 }, // Margin bottom on small screens
+            marginBottom: { xs: 2, md: 0 },
           }}
         >
-          MarkExams
+          Mark Exams
         </Typography>
 
-
-        <Box sx={
-          {
+        <Box
+          sx={{
             display: 'flex',
-            flexDirection: { xs: 'column', md: 'row' }, // Stack on small screens, row on medium and up
+            flexDirection: { xs: 'column', md: 'row' },
             justifyContent: 'space-between',
-
-          }
-        }>
+          }}
+        >
           <CustomInput />
 
           <div className="top-navbar-icons">
             <Avatar src={user?.profile_picture} sx={{ width: 40, height: 40, ml: 2 }} />
           </div>
-
         </Box>
-
-
       </Box>
-
-
 
       <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%', height: '50px', gap: 2 }}>
         <Button
@@ -233,19 +232,12 @@ const MarkExamPage: React.FC = () => {
       </Box>
 
       <Grid container spacing={4} sx={{ mb: 5 }}>
-
-        {/* Analytics Cards */}
-
         <Grid item xs={12} md={3}>
           <AnalyticsCard title="Total Exam Mark" count={dataCount} />
         </Grid>
-
       </Grid>
 
-
-
       <Box sx={{ padding: 2 }}>
-
         <CustomDataTable
           rows={data}
           columns={columns}
@@ -254,9 +246,31 @@ const MarkExamPage: React.FC = () => {
           isError={networkError}
           paginationModel={paginationModel}
           onPaginationModelChange={handlePaginationModelChange}
-
         />
       </Box>
+
+      <Dialog open={isModalOpen} onClose={handleCloseModal} fullWidth maxWidth="sm">
+        <DialogTitle>View Exam Details</DialogTitle>
+        <DialogContent>
+          {selectedRowData && (
+            <Box mt={1}>
+              <Stack spacing={2}>
+                <DetailRow label="Candidate Number" value={selectedRowData.candidate_number} />
+                <DetailRow label="Question" value={selectedRowData.question_text} />
+                <DetailRow label="Student Answer" value={selectedRowData.student_answer} />
+                <DetailRow label="Student Score" value={selectedRowData.student_score} />
+                <DetailRow label="Similarity Score" value={`${selectedRowData.similarity_score_percentage}`} />
+              </Stack>
+            </Box>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+
     </Box>
   );
 };
